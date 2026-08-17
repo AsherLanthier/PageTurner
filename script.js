43fbde0154b488d651797ffa4ecd785ed29fc46f
Asher <asher.p.lanthier@gmail.com>
2026-08-17 16:13:33 -0400

    Added files to repository
    These files have the functionality to select a pdf, display it (one page at a time), and switch back and forth between pages. Eventually, plan to add communication with arduino so that you are able to switch pages via a pedal

---
+++ script.js
@@ -0,0 +1,68 @@
+import * as pdfjsLib from 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/6.2.108/pdf.min.mjs';
+pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/6.2.108/pdf.worker.min.mjs';
+
+var fileChooser = document.getElementById('fileChooser');
+fileChooser.addEventListener('change', (event) => {
+    var file = event.target.files[0];
+    var fileName = file.name;
+
+    var url = URL.createObjectURL(file);
+    displayPDF(url);
+})
+
+function displayPDF(url) {
+    var previousPageButton = document.getElementById('previousPageButton');
+    var nextPageButton = document.getElementById('nextPageButton');
+
+    var loadingTask = pdfjsLib.getDocument({ url });
+    loadingTask.promise.then(function(pdf) {
+        console.log('PDF Loaded');
+        var pageNumber = 1
+        var minPages = 1;
+        var maxPages = pdf.numPages;
+
+        function renderPage(pageNumber) {
+            pdf.getPage(pageNumber).then(function(page) {
+                var scale = 1.5;
+                var viewport = page.getViewport({ scale: scale, });
+
+                var outputScale = window.devicePixelRatio || 1;
+
+                var canvas = document.getElementById("pdfCanvas");
+                var context = canvas.getContext("2d");
+
+                canvas.width = Math.floor(viewport.width * outputScale);
+                canvas.height = Math.floor(viewport.height * outputScale);
+                canvas.style.width = Math.floor(viewport.width) + "px";
+                canvas.style.height =  Math.floor(viewport.height) + "px";
+
+                var transform = outputScale !== 1
+                ? [outputScale, 0, 0, outputScale, 0, 0]
+                : null;
+
+                var renderContext = {
+                    canvasContext: context,
+                    transform: transform,
+                    viewport: viewport
+                };
+                page.render(renderContext);
+            })
+        }
+
+        previousPageButton.addEventListener('click', () => {
+            if (pageNumber != minPages) {
+                pageNumber --;
+                renderPage(pageNumber);
+            }
+        })
+
+        nextPageButton.addEventListener('click', () => {
+            if (pageNumber != maxPages) {
+                pageNumber ++;
+                renderPage(pageNumber);
+            }
+        })
+        
+        renderPage(pageNumber);
+    });
+}
